5ef8880c1bd7449ed55bc51bc7f1a1476a0e2c7a
dpgian <dplgian@gmail.com>
2022-11-14 13:56:26 +0000

    first commit

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import { useIsFocused } from '@react-navigation/core'
+import { StatusBar } from 'expo-status-bar'
+
+const FocusedStatusBar = (props) => {
+  const isFocused = useIsFocused()
+
+  return isFocused ? <StatusBar animated={true} {...props} /> : null
+}
+
+export default FocusedStatusBar
